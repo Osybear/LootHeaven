048f1c9d5b169186aed6ed59fc1c080da2561688
Osybear <osybeargames@gmail.com>
2018-08-10 23:51:12 -0500

    Revert "Update"
    This reverts commit 1ffc4651263f9bc6a31c70b2b893afd1f87a1c1a.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -19,9 +19,44 @@ public class Item : MonoBehaviour {
 		entry.callback.AddListener( (eventData) => { PointerDown(); } );
 		EventTrigger.triggers.Add(entry);
 
+		EventTrigger.Entry entry1 = new EventTrigger.Entry();
+		entry1.eventID = EventTriggerType.Drag;
+		entry1.callback.AddListener( (eventData) => { Drag(); } );
+		EventTrigger.triggers.Add(entry1);
+
+		EventTrigger.Entry entry2 = new EventTrigger.Entry();
+		entry2.eventID = EventTriggerType.EndDrag;
+		entry2.callback.AddListener( (eventData) => { EndDrag(); }  );
+		EventTrigger.triggers.Add(entry2);
+
 	}
 
 	public void PointerDown(){
-		Debug.Log("Move to Inventory");
+		m_DragOffset = transform.position - Input.mousePosition;
+		m_PreviousPosition = transform.position;
 	}
+	
+	public void Drag(){
+		transform.position = Input.mousePosition + m_DragOffset;
+	}
+
+	public void EndDrag(){
+		if(WithinLoot()){
+			transform.SetParent(m_LootHolder);
+		}else if(!WithinLoot()){
+			transform.position = m_PreviousPosition;
+		}
+	}
+
+	private bool WithinLoot(){
+		Vector3[] Corners = new Vector3[4];
+		m_LootHolder.GetWorldCorners(Corners);
+		Vector3 MousePos = Input.mousePosition;
+
+		if(MousePos.x > Corners[0].x && MousePos.y > Corners[0].y)
+			if(MousePos.x < Corners[2].x && MousePos.y < Corners[2].y)
+				return true;
+		return false;
+	}
+
 }
